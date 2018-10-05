add5d5cc80eb4f5787233833ad418b596e02d6dc
Erdem Derebaşoğlu <erdem.derebasoglu@vestel.com.tr>
2018-10-05 14:17:41 +0300

    use int instead of DrawCardMessage

--- handle_messages.lua
+++ handle_messages.lua
@@ -3,14 +3,12 @@ local nk = require("nakama")
 local mh = {}
 
 function mh.drawCard(context, dispatcher, tick, state, message)
-	local drawCardMessage = {
-		card = table.remove(state.deck)
-	}
+	local card = table.remove(state.deck)
 
 	local senderPresence = state.presences[message.sender.session_id];
-	table.insert(senderPresence.cards, drawCardMessage.card)
+	table.insert(senderPresence.cards, card)
 
-	dispatcher.broadcast_message(2, nk.json_encode(drawCardMessage), {senderPresence})
+	dispatcher.broadcast_message(2, nk.json_encode(card), {senderPresence})
 
 	dispatcher.broadcast_message(3, nk.json_encode(senderPresence.direction))
 end
